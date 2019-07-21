adbcbaafe406a06ee02684eec03c321017d99159
Deshar <deshar_wor@yahoo.com>
2019-07-21 05:40:16 -0700

    My first PyEz

--- test1.py
+++ test1.py
@@ -1,2 +1,17 @@
-print("Hello")
-print("Test more codes")
+
+from jnpr.junos import Device
+from jnpr.junos.op.fpc import FpcHwTable
+#from jnpr.junos.op.fpc import FpcMiReHwTable
+
+dev = Device(host='192.168.0.5', user='deshar', password='9miraihe')
+dev.open()
+
+fpcs = FpcHwTable(dev)
+fpcs.get()
+print(fpcs)
+
+for fpc in fpcs:
+         print (fpc.key," Description:", fpc.desc, "Model:", fpc.model,"Serial:", fpc.sn, "Part-number:", fpc.pn)
+
+
+dev.close()
