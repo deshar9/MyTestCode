d24522d8128120f9e719d4c1c7f100b11f750476
Deshar <deshar_wor@yahoo.com>
2019-07-22 05:42:39 -0700

    Edit to cover info table

--- test1.py
+++ test1.py
@@ -1,29 +1,25 @@
 from pprint import pprint
 from jnpr.junos import Device
 from jnpr.junos.op.fpc import FpcHwTable
-from jnpr.junos.op.fpc import FpcMiReHwTable
-from jnpr.junos.op.xcvr import XcvrTable
+from jnpr.junos.op.fpc import FpcInfoTable
 
 dev = Device(host='192.168.0.5', user='deshar', password='9miraihe')
 dev.open()
 
 fpcs = FpcHwTable(dev)
 fpcs.get()
-mics = FpcMiReHwTable(dev)
-mics.get()
-xcvrs = XcvrTable(dev)
-xcvrs.get()
+jfpcs = FpcInfoTable(dev)
+jfpcs.get()
+
 
 print(fpcs)
 pprint(fpcs.keys())
 pprint(fpcs.values())
 for fpc in fpcs:
          print (fpc.key," Description:", fpc.desc, "Model:", fpc.model,"Serial:", fpc.sn, "Part-number:", fpc.pn)
-pprint(mics)
-pprint(mics.keys())
-pprint(mics.values())
-pprint(xcvrs)
-pprint(xcvrs.keys())
-pprint(xcvrs.values())
+pprint(jfpcs)
+pprint(jfpcs.keys())
+pprint(jfpcs.values())
+
 
 dev.close()
